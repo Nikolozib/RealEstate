5cc368cba9021c261cd4a56d06d8b7588e5d07a4
Nikolozib <149243170+Nikolozib@users.noreply.github.com>
2026-06-08 16:47:56 +0400

    creatingCoreFilesAndDatabase

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,31 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth-guard';
 import { About } from './Components/about/about';
 import { Contact } from './Components/contact/contact';
 import { Favorites } from './Components/favorites/favorites';
 import { Home } from './Components/home/home';
 import { Listings } from './Components/listings/listings';
-import { Login } from './Components/login/login';
+import { Login } from './Components/Auth/login/login';
 import { NotFound } from './Components/not-found/not-found';
 import { Profile } from './Components/profile/profile';
 import { PropertyDetail } from './Components/property-detail/property-detail';
-import { Register } from './Components/register/register';
+import { Register } from './Components/Auth/register/register';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'about', component: About },
   { path: 'contact', component: Contact },
-  { path: 'favorites', component: Favorites },
+  { path: 'favorites', component: Favorites, canActivate: [authGuard] },
   { path: 'listings', component: Listings },
-  { path: 'login', component: Login },
-  { path: 'profile', component: Profile },
-  { path: 'property-detail', component: PropertyDetail },
-  { path: 'register', component: Register },
+  {
+    path: 'auth',
+    children: [
+      { path: 'login', component: Login },
+      { path: 'register', component: Register },
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+    ],
+  },
+  { path: 'profile', component: Profile, canActivate: [authGuard] },
+  { path: 'property-detail', component: PropertyDetail, canActivate: [authGuard] },
   { path: '**', component: NotFound },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './Components/shared/footer/footer';
+import { Header } from './Components/shared/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, RouterOutlet, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
